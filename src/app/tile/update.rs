36504fed77d6ca2fc571e08f1d87b4f752ba03a9
unsecretised <no1umang@gmail.com>
2026-03-27 16:17:16 +0800

    reset shell commands on default button

--- src/app/tile/update.rs
+++ src/app/tile/update.rs
@@ -703,7 +703,6 @@ pub fn handle_update(tile: &mut Tile, message: Message) -> Task<Message> {
                 }
                 SetConfigFields::ToDefault => {
                     final_config = Config::default();
-                    final_config.shells = tile.config.shells.clone();
                 }
             };
 
